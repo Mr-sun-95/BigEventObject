305999a1ef18aac7b1f19ad83b14d8ad9984717c
swz <13186145992@163.com>
2020-09-11 11:25:55 +0800

    完成主页退出进入功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,25 @@ $.ajaxPrefilter(function (options) {
     // 在发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
     console.log(options.url);
+
+    // 统一为有权限的接口，设置headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局统一挂载 complete 回调函数
+    // 无论成功失败都会执行complete函数
+    options.complete = function (res) {
+        // console.log('res回调');
+        console.log(res);
+        // 在 complete 回调函数中，可以使用 res.response.JSON 拿到服务器返回的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1. 强制清空假token
+            localStorage.removeItem('token')
+            // 2.跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$(function () {
+    // 调用getUserInfo
+    getUserInfo()
+    // 点击按钮，实现退出功能
+    $('#btn_Logout').on('click', function () {
+        layer.confirm('确认退出登录?', { icon: 5, title: '提示' }, function (index) {
+            //do something
+            // 1.清空本地存储token
+            localStorage.removeItem('token')
+            // 2.跳到登录页面
+            location.href = '/login.html'
+
+            // 关闭confirm框
+            layer.close(index);
+        });
+    })
+
+
+    function getUserInfo() {
+        $.ajax({
+            url: '/my/userinfo',
+            // headers 就是请求头配置对象
+            // headers: {
+            //     Authorization: localStorage.getItem('token') || ''
+            // },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取用户信息失败')
+
+                }
+                // 调用renderAvatar 渲染用户头像
+                renderAvatar(res.data)
+            },
+
+        })
+    }
+    // 渲染用户头像
+    function renderAvatar(user) {
+        // 1.获取用户名称
+        var name = user.nickname || user.username
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        if (user.user_pic !== null) {
+            $('.layui-nav-img').attr('src', user.user_pic).show()
+            $('.text-avatar').hide()
+        } else {
+            $('.layui-nav-img').hide()
+            var first = name[0].toUpperCase()
+            $('.text-avatar').html(first).show()
+        }
+    }
+
+})
